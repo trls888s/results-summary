be3e35530f71ee202679d366257939d2faff154a
trls420s <110872148+trls420s@users.noreply.github.com>
2023-05-15 20:26:29 +1200

    new file:   public/assets/images/favicon-32x32.png 	new file:   public/assets/images/icon-memory.svg 	new file:   public/assets/images/icon-reaction.svg 	new file:   public/assets/images/icon-verbal.svg 	new file:   public/assets/images/icon-visual.svg 	modified:   public/index.html 	modified:   src/App.css 	modified:   src/App.js 	new file:   src/data.json

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
+const myData = require('./data.json');
+
+function Info({id}) {
+  //console.log({myData[id].icon});
+  return (
+    <>
+    <img src={myData[id].icon} alt="FEM Icon" /> {myData[id].category} {myData[id].score}
+    </>
+  );
+}
+
+function TotalScore() {
+  let scoreA = 0;
+  for (let i = 0; i < myData.length; i++) {
+    scoreA += myData[i].score;
+  };
+  let totalScore = scoreA/myData.length;
+  console.log(totalScore);
+  return (
+    <>
+    {totalScore}
+    </>
+    );
+}
+
 
 function App() {
   return (
+    <div className="container">
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className='total_score'>
+      <div className='ts_div scorenum'>Your Result</div>
+    <div className="scorecircle"><span className='scorenum'><TotalScore /></span>
+    <p>of 100</p></div>
+    <div className='ts_div'>Your Result is better than 65% of People</div>
+    
+    
+    
+    </div>
+    
+    <div className='output'>
+      Summery
+    <div className='info'><Info id='0' /></div>
+    <div className='info'><Info id='1' /></div>
+    <div className='info'><Info id='2' /></div>
+    <div className='info'><Info id='3' /></div>
+    <div className='continue'>Continue</div>
+    </div>
+    </div>
     </div>
   );
 }
