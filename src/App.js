33e24ce2ab1acff988efff7bed79076a5ddb2e77
trls420s <110872148+trls420s@users.noreply.github.com>
2023-05-19 13:15:03 +1200

    modified:   src/App.css 	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function Info({id}) {
   //console.log({myData[id].icon});
   return (
     <>
-    <span className='infologo'><img src={myData[id].icon} alt="FEM Icon" /> {myData[id].category} </span><span className='infoscore'>{myData[id].score}</span>
+    <div className='infologo'><img src={myData[id].icon} alt="FEM Icon" /> {myData[id].category} </div><div className='infoscore'>{myData[id].score} / 100</div>
     </>
   );
 }
@@ -32,7 +32,7 @@ function App() {
     
     <div className='total_score'>
       <div className='ts_div scorenum'>Your Result</div>
-    <div className="scorecircle"><span className='scorenum'><TotalScore /></span>
+    <div className="scorecircle"><p><div className='scorenum'><TotalScore /></div></p>
     <p>of 100</p></div>
     <div className='ts_div'>Your Result is better than 65% of People conducting the test here!</div>
     
@@ -41,8 +41,8 @@ function App() {
     </div>
     
     <div className='output'>
-      Summary
-    <div className='info'><Info id='0' /></div>
+      <div className="summary">Summary</div>
+    <div className='info reaction'><Info id='0' /></div>
     <div className='info memory'><Info id='1' /></div>
     <div className='info verbal'><Info id='2' /></div>
     <div className='info visual'><Info id='3' /></div>
